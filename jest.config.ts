feac96ff6434ca96de60aecf8e31dee7e43c9a28
saleem sabeihe <101823359+nareens@users.noreply.github.com>
2024-07-27 12:43:11 +0300

    fixed tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,13 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    // Add other configurations as needed
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/client/',  // Add this line to ignore the client directory
+  ],
+  // Add other configurations as needed
 };
 
 export default config;
