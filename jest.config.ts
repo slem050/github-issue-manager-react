fccecc50922f8eb9b08d408a6b55676165b5a8b5
shami544 <bnb544bnb@gmail.com>
2024-07-27 12:21:59 +0300

    added test script

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    // Add other configurations as needed
+};
+
+export default config;
